7c6d180ca4f5c5a3e70f462fca9c708949e79600
sardo.ip <sardo.ip@nxtomo.hk>
2015-12-10 16:00:52 +0800

    Can get plurk and response by date, and write it to a html file

--- crawler.rb
+++ crawler.rb
@@ -1,7 +1,7 @@
-#encoding: UTF-8
+# encoding: UTF-8
 
 require 'rubygems'
-require "tjplurker"
+require 'tjplurker'
 
 include TJP
 
@@ -13,22 +13,40 @@ consumerSecert = setting['consumerSecert']
 accessKey = setting['accessKey']
 accessSecret = setting['accessSecret']
 
-initTime = DateTime.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, 0, 0, 0, 0)
+startTime = DateTime.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, 0, 0, 0, 0)
 
-initTimeMs = initTime.strftime('%Q')
+startTimeMs = startTime.strftime('%Q')
+
+endTime = DateTime.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, 23, 59, 59, 59)
+
+endTimeMs = endTime.strftime('%Q')
 
 tjp = TJPlurker.new(consumerKey, consumerSecert, accessKey, accessSecret)
 
-userId = get_uid("bluewinds0624").to_i
+userId = get_uid('bluewinds0624').to_i
 
-publicPlurks = tjp.api("/APP/Timeline/getPublicPlurks",{user_id: userId})['plurks']
+publicPlurks = tjp.api('/APP/Timeline/getPublicPlurks', user_id: userId)['plurks']
 
 publicPlurks.each do |publicPlurk|
 
-	publicPlurkId = publicPlurk['plurk_id']
+	postedDate = DateTime.httpdate(publicPlurk['posted']).strftime('%Y%m%d%H%M%S')
+	postedDateMs = DateTime.httpdate(publicPlurk['posted']).strftime('%Q')
 
-	postedDate = DateTime.httpdate(publicPlurk['posted']).strftime('%Q')
+	if postedDateMs > startTimeMs and postedDateMs < endTimeMs
 
-	responses = tjp.api("/APP/Responses/get",{plurk_id: publicPlurkId})['responses']
+		fileHtml = File.new("./output/#{postedDate}.html", 'w')
+		fileHtml.puts '<html><body><head><meta charset="UTF-8"></head>'
 
+		publicPlurkId = publicPlurk['plurk_id']
+
+		responses = tjp.api('/APP/Responses/get', plurk_id: publicPlurkId)['responses']
+
+		responses.each do |response|
+			fileHtml.puts response['content']
+			fileHtml.puts '</br>'
+		end
+
+		fileHtml.puts '</body></html>'
+		fileHtml.close
+	end
 end
